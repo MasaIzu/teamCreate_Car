f60f5f61310fd5db3bcd590bfccd0666fb88f344
LC1B_04_Izumida_Masato <k021g1329@g.neec.ac.jp>
2022-10-06 14:31:49 +0900

    affinのファイル移動

---
+++ math/Matrix4.cpp
@@ -0,0 +1,21 @@
+#include"Matrix4.h"
+#include"Vector3.h"
+
+// ２項演算子　*　のオーバーロード関数（行列と行列の積）
+Matrix4 Matrix4::operator*(const Matrix4& m1)
+{
+	return *this *= m1;
+}
+
+// ２項演算子　*　のオーバーロード関数（ベクトルと行列の積）
+Vector3 operator*(const Vector3& v, const Matrix4& m)
+{
+	float W = v.x * m.m[0][3] + v.y * m.m[1][3] + v.z * m.m[2][3] + m.m[3][3];
+	Vector3 result
+	{
+		(v.x * m.m[0][0] + v.y * m.m[1][0] + v.z * m.m[2][0] + m.m[3][0]) / W,
+		(v.x * m.m[0][1] + v.y * m.m[1][1] + v.z * m.m[2][1] + m.m[3][1]) / W,
+		(v.x * m.m[0][2] + v.y * m.m[1][2] + v.z * m.m[2][2] + m.m[3][2]) / W
+	};
+	return result;
+}
