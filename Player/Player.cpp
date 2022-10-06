22c61059cacb136a51e02ea69aaa8609518b7578
LC1B_04_Izumida_Masato <k021g1329@g.neec.ac.jp>
2022-10-06 13:43:34 +0900

    プレイヤーの追加

---
+++ Player/Player.cpp
@@ -0,0 +1,22 @@
+#include "Player.h"
+
+Player::Player()
+{
+}
+
+void Player::Initialize()
+{
+}
+
+void Player::Updata()
+{
+}
+
+void Player::Draw()
+{
+}
+
+float Player::GetPlayerSpeed()
+{
+    return 0.0f;
+}
---
+++ Player/Player.h
@@ -0,0 +1,35 @@
+#pragma once
+
+class Player {
+
+public:
+    Player();
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    void Initialize();
+
+    /// <summary>
+    /// 更新
+    /// </summary>
+    void Updata();
+
+    /// <summary>
+    /// 描画
+    /// </summary>
+    void Draw();
+
+    /// <summary>
+    /// ゲッター
+    /// </summary>
+    float GetPlayerSpeed();
+
+
+
+
+private:
+    //メンバ変数
+
+
+};
