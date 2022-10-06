d050563a3ab05a21b3bff9581eec75a40a968b1d
YuyaHiguchi <k021g1263@g.neec.ac.jp>
2022-10-06 14:43:07 +0900

    Enemyクラスの中身を少し追加とEnemyPopクラスを作成

--- Enemy/Enemy.cpp
+++ Enemy/Enemy.cpp
@@ -1,1 +1,46 @@
 #include "Enemy.h"
+
+void Enemy::Initialize(Model* model ,Vector3& pos, CarModel carModel)
+{
+	// NULLポインタチェック
+	assert(model);
+
+	model_ = model;
+
+	// ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+
+	// 初期座標を設定
+	worldTransform_.translation_ = pos;
+
+	// 車種を設定
+	carModel_ = carModel;
+
+	// 車種ごとのスピードを設定
+	switch (carModel_)
+	{
+	case CarModel::truck:
+		moveSpeed_ = { 0.6f,0.6f, 0.6f };
+		break;
+	case CarModel::prius:
+		moveSpeed_ = { 1.0f,1.0f, 1.0f };
+		break;
+	case CarModel::ferrari:
+		moveSpeed_ = { 1.6f,1.6f, 1.6f };
+		break;
+	default:
+		break;
+	}
+}
+
+void Enemy::Update()
+{
+	
+}
+
+void Enemy::Draw(const ViewProjection& viewProjection)
+{
+	if (isDead_ == false) {
+		model_->Draw(worldTransform_, viewProjection, textureHandle_);
+	}
+}
--- Enemy/Enemy.h
+++ Enemy/Enemy.h
@@ -1,5 +1,51 @@
 #pragma once
+#include "Model.h"
+#include "WorldTransform.h"
+#include <cassert>
+
+enum class CarModel {
+	truck,// トラック（遅い車）
+	prius,// プリウス（一般車）
+	ferrari,// フェラーリ（速い車）
+};
+
 class Enemy
 {
+public:// メンバ関数
+
+	// 初期化処理関数
+	void Initialize(Model* model ,Vector3& pos, CarModel carModel);
+
+	// 更新処理関数
+	void Update();
+
+	// 描画処理関数
+	void Draw(const ViewProjection& viewProjection);
+
+	// 死亡フラグを渡す
+	bool IsDead() const { return isDead_; }
+
+private:// メンバ変数
+
+	// ワールド変換データ
+	WorldTransform worldTransform_;
+
+	//モデル
+	Model* model_ = nullptr;
+
+	// 速度全般
+	// 進む速度
+	Vector3 moveSpeed_ = { 1.0f,1.0f,1.0f };
+	// 回転速度
+	Vector3 rotationSpeed_ = { 0.1f,0.1f, 0.1f };
+
+	// デスフラグ
+	bool isDead_ = false;
+
+	// 車のパターン
+	CarModel carModel_ = CarModel::prius;
+
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
 };
 
