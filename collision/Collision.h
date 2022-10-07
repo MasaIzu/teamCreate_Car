dd60a4fcff0b505c0a2b64ab0986562b6885be0c
一真 田代 <t.kazuma.0809@gmail.com>
2022-10-08 00:01:52 +0900

    当たり判定の実装

--- collision/Collision.h
+++ collision/Collision.h
@@ -1,1 +1,28 @@
 #pragma once
+#include "WorldTransform.h"
+#include "Model.h"
+#include <cassert>
+#include "DebugText.h"
+#include "Input.h"
+#include "ViewProjection.h"
+#include <memory>
+#include <list>
+
+class collision {
+public:
+    //プレイヤーとエネミーの座標を入る
+    //プレイヤーの半径をx,y,zの順に入れる
+    //エネミーの半径をx,y,zの順に入れる
+    bool boxCollision(Vector3 player, Vector3 enemy, float playerWidthX, float playerWidthZ, float playerHight, float enemyWidthX, float enemyWidthZ, float enemyHight);
+private:
+    //ワールド変換データ
+    WorldTransform worldTransform;
+
+    DebugText* debugText = nullptr;
+    //モデル
+    Model* model = nullptr;
+    //モデル
+    Model* enemyBulletModel = nullptr;
+
+    
+};
