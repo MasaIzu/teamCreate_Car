24b23cf7dc7474860cbff765d34ff2ef85debae9
NishizawaYuga <k021g1246@g.neec.ac.jp>
2022-10-09 15:42:03 +0900

    数増やした

--- Load.cpp
+++ Load.cpp
@@ -10,9 +10,9 @@ void Load::Initialize(Model* model) {
 	model_ = model;
 
 	//初期座標をセット
-	for (int i = 0; i < 7; i++) {
+	for (int i = 0; i < 12; i++) {
 		float z = i * 39;
-		worldTransform_[i].translation_ = Vector3{0,-50,z};
+		worldTransform_[i].translation_ = Vector3{0,-50,z + 390};
 
 		//ワールド変換の初期化
 		worldTransform_[i].Initialize();
@@ -27,11 +27,11 @@ void Load::Update() {
 	//更新用の変数
 	MatSet2 matSet;
 
-	for (int i = 0; i < 7; i++) {
+	for (int i = 0; i < 12; i++) {
 		worldTransform_[i].translation_.z -= speed;
 
 		if (worldTransform_[i].translation_.z < -90) {
-			worldTransform_[i].translation_.z = 180;
+			worldTransform_[i].translation_.z = 370;
 		}
 
 		//ワールドトランスフォームの更新
@@ -42,7 +42,7 @@ void Load::Update() {
 
 void Load::Draw(ViewProjection viewProjection) {
 	//3Dモデルを描画
-	for (int i = 0; i < 7; i++) {
+	for (int i = 0; i < 12; i++) {
 		model_->Draw(worldTransform_[i], viewProjection);
 	}
 }
