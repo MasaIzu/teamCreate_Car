4dea38bf9ef18c8506deab2c6f1c4d3b47b4d98c
一真 田代 <t.kazuma.0809@gmail.com>
2022-10-09 15:45:28 +0900

    collision直し

--- collision/collision.cpp
+++ collision/collision.cpp
@@ -4,31 +4,31 @@ bool collision::boxCollision(Vector3 player, Vector3 enemy, float playerWidthX, 
 {
 	
 	//ÉvÉåÉCÉÑÅ[
-	float pXMin = player.x - playerWidthX / 2;
+	float pXMin = player.x - playerWidthX;
 
-	float pXMax = player.x + playerWidthX / 2;
+	float pXMax = player.x + playerWidthX;
 	
-	float pYMin = player.y - playerHight / 2;
+	float pYMin = player.y - playerHight;
 
-	float pYMax = player.y + playerHight / 2;
+	float pYMax = player.y + playerHight;
 
-	float pZMin = player.z - playerWidthZ / 2;
+	float pZMin = player.z - playerWidthZ;
 
-	float pZMax = player.z + playerWidthZ / 2;
+	float pZMax = player.z + playerWidthZ;
 	
 	//ìG
 	//ïΩñ 
-	float eXMin = enemy.x - enemyWidthX / 2;
+	float eXMin = enemy.x - enemyWidthX;
 
-	float eXMax = enemy.x + enemyWidthX / 2;
+	float eXMax = enemy.x + enemyWidthX;
 
-	float eYMin = enemy.y - enemyHight / 2;
+	float eYMin = enemy.y - enemyHight;
 
-	float eYMax = enemy.y + enemyHight / 2;
+	float eYMax = enemy.y + enemyHight;
 
-	float eZMin = enemy.z - enemyWidthZ / 2;
+	float eZMin = enemy.z - enemyWidthZ;
 
-	float eZMax = enemy.z + enemyWidthZ / 2;
+	float eZMax = enemy.z + enemyWidthZ;
 
 	if ((pXMin <= eXMax && pXMax >= eXMin) && (pYMin <= eYMax && pYMax >= eYMin) && (pZMin <= eZMax && pZMax >= eZMin)) {
 		return true;
