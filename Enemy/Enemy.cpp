698b4725788cf33ebb9d2c5871e03a4fa3ead8b9
masato izumida <Oioi_0126@outlook.com>
2022-10-25 02:58:21 +0900

    a

--- Enemy/Enemy.cpp
+++ Enemy/Enemy.cpp
@@ -178,7 +178,7 @@ void Enemy::Draw(const ViewProjection& viewProjection)
 void Enemy::ContactPlayer()
 {
 	if (contactFlag == false) {
-		if (collision_->boxCollision(player_->GetPlayerPos(), worldTransform_.translation_, Vector3(5, 5, 5), Vector3(5, 5, 5))) {
+		if (collision_->boxCollision(player_->GetPlayerPos(), worldTransform_.translation_, Vector3(5, 2, 5), Vector3(5, 6, 6))) {
 			if (player_->GetMovingFlag() == 0) {
 				fukitobiTime = fukitobiTimeMax;
 				contactVer0 = true;
@@ -187,7 +187,7 @@ void Enemy::ContactPlayer()
 		}
 	}
 	if (contactFlag == false) {
-		if (collision_->boxCollision(player_->GetPlayerPos(), worldTransform_.translation_, Vector3(5, 5, 5), Vector3(5, 5, 5))) {
+		if (collision_->boxCollision(player_->GetPlayerPos(), worldTransform_.translation_, Vector3(5, 2, 5), Vector3(5, 6, 6))) {
 			if (player_->GetMovingFlag() == 1) {
 				contactVer1 = true;
 			}
